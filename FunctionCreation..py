928248c06b671ceb8aed08c1cc9c1ef220b5725b
kamuleva <kamuleva@gmail.com>
2020-07-21 11:20:07 +0530

    fuction

---
+++ FunctionCreation..py
@@ -0,0 +1,14 @@
+def add( a, b):
+    return a+b
+def greeting(name):
+    print("Hello Dear !!!", name, " Good Morning")
+
+myres=add(2,4)
+print(myres)
+myres=add(2,5)
+print(myres)
+myres=add(2,7)
+print(myres)
+myres=add(2,8)
+print(myres)
+greeting("KRISHNAM")
